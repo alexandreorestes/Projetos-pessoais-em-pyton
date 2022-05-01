5358ed270d8cb45f84d633c10882cb05a10b318e
ale3729 <alexandre.ufu@gmail.com>
2022-05-01 14:12:22 -0300

    Update capturaCifra.py

--- capturaCifra.py
+++ capturaCifra.py
@@ -10,7 +10,7 @@ covert_cifra = str(cifra[0])
 
 
 with open('copia_de-cifra.txt', 'w' ) as arquivo:
-    arquivo.write('<div align="left"><button type="button" onclick="baixar()">- ½</button><button onclick="location.reload()"> Tom </button><button type="button" onclick="subir()">+ ½</button></div>\n')
+    arquivo.write('<div class="container"><div class="row"><div class="col-sm-8"><iframe width="360" height="200" src=" " frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div><div class="col-sm-4">Transpor<div class="btn-group" role="group"><button type="button" onclick="baixar()">-</button><button onclick="location.reload()">½</button><button type="button" onclick="subir()">+</button></div></div></div></div>\n')
     for linhas in covert_cifra:
         arquivo.write(linhas)
     arquivo.write('\n\n<script src="/wp-admin/js/transpor.js"></script>')
