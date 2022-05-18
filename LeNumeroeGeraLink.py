b62462c773975e80095f11b6a2b769e31b759af0
ale3729 <alexandre.ufu@gmail.com>
2022-05-18 11:44:11 -0300

    Add files via upload

---
+++ LeNumeroeGeraLink.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+arquivo = open('Lista2.txt', 'r')
+for numero in range(350):
+    numero = arquivo.readline()
+    pre_link = 'https://wa.me/'
+    link = pre_link + numero
+    print(link)
+
+
+
