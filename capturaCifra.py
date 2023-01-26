c14c1ff63c03586dd2b942119a23cb589deed29c
Alexandre Orestes Alves <alexandre.ufu@gmail.com>
2023-01-26 10:49:01 -0300

    Update capturaCifra.py
    adicionei cores azul ao botão

--- capturaCifra.py
+++ capturaCifra.py
@@ -1,3 +1,4 @@
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -10,8 +11,7 @@ covert_cifra = str(cifra[0])
 
 
 with open('copia_de-cifra.txt', 'w' ) as arquivo:
-    arquivo.write('<div class="container"><div class="row"><div class="col-sm-8"><iframe width="360" height="200" src=" " frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div><div class="col-sm-4">Transpor<div class="btn-group" role="group"><button type="button" onclick="baixar()">-</button><button onclick="location.reload()">½</button><button type="button" onclick="subir()">+</button></div></div></div></div>\n')
+    arquivo.write('<div class="container"><div class="row"><div class="col-sm-8"><iframe width="360" height="200" src=" " frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div><div class="col-sm-4">Transpor<div class="btn-group" role="group"><button type="button" style="border-radius:10%; background-color:blue; color:white; border:none;" onclick="baixar()">-</button><button style="border-radius:10%; background-color:blue; color:white; border:none;" onclick="location.reload()">½</button><button type="button" style="border-radius:10%; background-color:blue; color:white; border:none;" onclick="subir()">+</button></div></div>\n')
     for linhas in covert_cifra:
         arquivo.write(linhas)
     arquivo.write('\n\n<script src="/wp-admin/js/transpor.js"></script>')
-
